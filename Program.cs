0e6cb01739c8655db445691564145caee23ca962
Yulia <Lila2763@yandex.ru>
2022-12-09 16:03:20 +0300

    FinalProgramm

--- Program.cs
+++ Program.cs
@@ -18,6 +18,22 @@ void PrintArray(string[] array)
     Console.WriteLine();
 }
 
+void ThreeChar(string[] array1, string[] array2 )
+{
+    int element = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {        
+        if (array1[i].Length <= 3 )
+        {
+            array2[element] = array1[i];
+            element++;
+        }
+    }
+}
+
 string[] newArray = new string[] { "Hello", "2", "world", ":-)", "1234", "1567", "-2" };
+string[] newArrayTwo = new string[newArray.Length];
 
 PrintArray(newArray);
+ThreeChar(newArray,newArrayTwo);
+PrintArray(newArrayTwo);
